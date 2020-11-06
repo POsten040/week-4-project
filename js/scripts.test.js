b6ace95a980671d42168320a6acbc75182004925
Patrick Osten <Patrickosten040@gmail.com>
2020-11-06 09:45:44 -0800

    display price of pizza in html after form submit

--- js/scripts.test.js
+++ js/scripts.test.js
@@ -9,8 +9,8 @@ function Pizza (size, T1, T2, T3, T4) {
 }
 
 Pizza.prototype.price = function() {
-  let cost = this.size * (this.T1 + this.T2 + this.T3 + this.T4);
-  return (cost + "" + "bucks"); 
+  let cost = 2 * (this.size * (this.T1 + this.T2 + this.T3 + this.T4));
+  return (cost + " " + "bucks"); 
 }
 
 // UI Logic
@@ -18,6 +18,7 @@ $(document).ready(function() {
   $("#formOne").submit(function(event) {
     event.preventDefault();
     let pizza = new Pizza(parseInt($("#size").val()), parseInt($("#cheese").val()), parseInt($("#meats").val()), parseInt($("#veggies").val()), parseInt($("#other").val()));
-    console.log(pizza);
-  })
-})
+    console.log(pizza.price());
+    $("#result").html("<h2>" + pizza.price() + "</h2>");
+  });
+});
