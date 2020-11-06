34fa28a299f9fad5ddcc0b760833f266594f5371
Patrick Osten <Patrickosten040@gmail.com>
2020-11-06 09:01:42 -0800

    tie inputs from form to Pizza constructor

--- js/scripts.test.js
+++ js/scripts.test.js
@@ -1,3 +1,5 @@
+// Business Logic
+
 function Pizza (size, T1, T2, T3, T4) {
   this.size = size;
   this.T1 = T1;
@@ -5,3 +7,12 @@ function Pizza (size, T1, T2, T3, T4) {
   this.T3 = T3;
   this.T4 = T4;
 }
+
+// UI Logic
+$(document).ready(function() {
+  $("#formOne").submit(function(event) {
+    event.preventDefault();
+    let pizza = new Pizza($("#size").val(), $("#cheese").val(), $("#meats").val(), $("#veggies").val(), $("#other").val());
+    console.log(pizza);
+  })
+})
